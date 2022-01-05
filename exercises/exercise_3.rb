c26e0c045e19fbde72e789125ce3ca8d0fb32bcb
BlesMateo <blesildaangelicamateo@gmail.com>
2022-01-05 12:33:35 -0500

    Deleted the third store from the database

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,21 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
+#Exercise 3: Delete the third store
+
+#Load the third store (into @store3) as you did the other two before.
+#Using Active Record's destroy method, delete the store from the database.
+#Verify that the store has been deleted by again outputting (putsing) the count
+#(as you did in Exercise 1.)
+
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Store Count = #{Store.count}"
+
+@store3 = Store.find(3)
+@store3.destroy
+
+puts "Store Count = #{Store.count}"
